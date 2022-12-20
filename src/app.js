028e461533c6469f724d39946a79e0c56dc1a06a
nwekeVictory <120378635+nwekeVictory@users.noreply.github.com>
2022-12-20 12:14:47 +0100

    Added search engine

--- src/app.js
+++ src/app.js
@@ -36,14 +36,24 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute("src",
+  iconElement.setAttribute(
+    "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-``
+function search(city) {
 let apiKey = "1de36ae5f38e4644748b891018ad8ef7";
-let city = "Lagos";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handlesubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("Lagos");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handlesubmit);
