9b4ae13bf66b2b1ec9688e350865abea82ebb2c5
nwekeVictory <120378635+nwekeVictory@users.noreply.github.com>
2022-12-25 00:59:16 +0100

    Tried adding the forecast days

--- src/app.js
+++ src/app.js
@@ -21,6 +21,45 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay) {
+    forecastHTML =
+      forecastHTML +
+      ` 
+   <div class="col-2">
+    <div class="weather-forecast-date">
+        ${forecastDay.dt}</div>
+        <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}2x.png"
+        alt="" 
+        width="42"
+         />
+      <div class="weather-forecast-temperatures">
+         <span class="weather-forecast- temperature-max">
+            ${forecastDay.temp.max}°</span>
+         <span class="weather-forecast-temperature-min">
+        ${forecastDay.temp.min}°</span>
+      </div>
+    </div>
+   `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKEy = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKEy}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
@@ -43,7 +82,10 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
+
 function search(city) {
   let apiKey = "1de36ae5f38e4644748b891018ad8ef7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
