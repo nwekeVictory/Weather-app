5efa6bdc43250269e8c634c4f3cda7060b753a1a
nwekeVictory <120378635+nwekeVictory@users.noreply.github.com>
2022-12-26 01:09:10 +0100

    Displayed the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ function formatDate(timestamp) {
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
-  };
+  }
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
-  };
+  }
   let days = [
     "Sunday",
     "Monday",
@@ -19,47 +19,58 @@ function formatDate(timestamp) {
   ];
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
-};
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "TUe", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
 
 function displayForecast(response) {
   let forecast = response.data.daily;
-  console.log(response.data.condition);
+  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
 
-  forecast.forEach(function (forecastDay) {
+  forecast.forEach(function (forecastDay , index) {
+    if (index < 6) {
     forecastHTML =
       forecastHTML +
       ` 
    <div class="col-2">
     <div class="weather-forecast-date">
-        ${forecastDay.dt}
+        ${formatDay(forecastDay.time)}
     </div>
-        <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}2x.png"
+        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+          forecastDay.condition.icon
+        }.png",
         alt="" 
         width="42"
          />
       <div class="weather-forecast-temperatures">
          <span class="weather-forecast- temperature-max">
-            ${forecastDay.temp.max}°</span>
+            ${Math.round(forecastDay.temperature.maximum)}°</span>
          <span class="weather-forecast-temperature-min">
-        ${forecastDay.temp.min}°</span>
+        ${Math.round(forecastDay.temperature.minimum)}°</span>
       </div>
     </div>
-   `;
+   `; }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-};
+}
 
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "fo4c0a1dd0d32b2efb14e97b788efft0";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.lon}&lat=${coordinates.lat}&key=${apiKeys}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.lon}&lat=${coordinates.lat}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
-};
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -85,19 +96,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   getForecast(response.data.coord);
-};
+}
 
 function search(city) {
   let apiKey = "1de36ae5f38e4644748b891018ad8ef7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
-};
+}
 
 function handlesubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
-};
+}
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
@@ -105,7 +116,7 @@ function displayFahrenheitTemperature(event) {
   fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-};
+}
 
 function displayCelsiusTemperature(event) {
   event.preventDefault();
@@ -113,10 +124,9 @@ function displayCelsiusTemperature(event) {
   fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celciusTemperature);
-};
+}
 
 let celciusTemperature = null;
-
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handlesubmit);
